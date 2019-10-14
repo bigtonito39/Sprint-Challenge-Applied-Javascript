87b90431cb7970507861f51bac97b33cbb054289
bigtonito39 <bigtonito@hotmail.com>
2019-10-14 17:58:09 -0400

    Bugs are now fixed, will be working on strech challenge

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,58 +1,57 @@
 // STEP 3: Create Article cards.
+
 // -----------------------
+
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+
 // Stduy the response data you get back, closely.
+
 // You will be creating a component for each 'article' in the list.
+
 // This won't be as easy as just iterating over an array though.
+
 // Create a function that will programmatically create the following DOM component:
+
 //
+
 // <div class="card">
+
 //   <div class="headline">{Headline of article}</div>
+
 //   <div class="author">
+
 //     <div class="img-container">
+
 //       <img src={url of authors image} />
+
 //     </div>
+
 //     <span>By {authors name}</span>
+
 //   </div>
+
 // </div>
+
 //
+
 // Create a card for each of the articles and add the card to the DOM.
+
 const cardContainer = document.querySelector(".cards-container")
 
+function cardCreater(data1) {
 
+    data1.bootstrap.forEach((bootvalue) => {
 
-function cardCreater(data) {
-    const cardDiv1 = document.createElement("div");
-    const cardDiv2 = document.createElement("div");
-    const cardDiv3 = document.createElement("div");
-    const cardDiv4 = document.createElement("div");
-    const cardImg = document.createElement("img");
-    const cardSpan1 = document.createElement("span");
-
-    cardDiv1.classList.add("card");
-
-
-    cardDiv2.classList.add("headline");
-    cardDiv3.classList.add("author");
-    cardDiv4.classList.add("img-container");
-
-
-    cardDiv1.appendChild(cardDiv2);
-    cardDiv1.appendChild(cardDiv3);
-    cardDiv3.appendChild(cardDiv4);
-    cardDiv4.appendChild(cardImg);
-    cardDiv3.appendChild(cardSpan1);
-    cardContainer.appendChild(cardDiv1)
-
-    for (let key in data.bootstrap) {
         const cardDiv1 = document.createElement("div");
+        cardDiv1.classList.add("card");
+        cardContainer.appendChild(cardDiv1)
+
+
         const cardDiv2 = document.createElement("div");
         const cardDiv3 = document.createElement("div");
         const cardDiv4 = document.createElement("div");
         const cardImg = document.createElement("img");
         const cardSpan1 = document.createElement("span");
-
-        cardDiv1.classList.add("card");
 
 
         cardDiv2.classList.add("headline");
@@ -60,95 +59,156 @@ function cardCreater(data) {
         cardDiv4.classList.add("img-container");
 
 
+
         cardDiv1.appendChild(cardDiv2);
         cardDiv1.appendChild(cardDiv3);
         cardDiv3.appendChild(cardDiv4);
         cardDiv4.appendChild(cardImg);
         cardDiv3.appendChild(cardSpan1);
+
+        cardDiv2.textContent = `${bootvalue.headline}`;
+        cardImg.setAttribute("src", bootvalue.authorPhoto)
+        cardSpan1.textContent = ` by ${bootvalue.authorName}`
+
+    });
+    data1.javascript.forEach((javascriptvalue) => {
+        const cardDiv1 = document.createElement("div");
+        cardDiv1.classList.add("card");
         cardContainer.appendChild(cardDiv1)
 
-        cardDiv2.textContent = `${data.bootstrap[key].headline}`;
-        cardDiv3.textContent = `${data.bootstrap[key].authorName}`
-        cardDiv4.textContent = `${data.bootstrap[key].headline}`;
-        cardSpan1.textContent = `${data.bootstrap[key].authorName}`
-        cardImg.setAttribute("src", data.bootstrap[key].authorPhoto)
 
-    }
-
-
-    /*for (let [key, value] of Object.entries(data.bootstrap)) {
+        const cardDiv2 = document.createElement("div");
+        const cardDiv3 = document.createElement("div");
+        const cardDiv4 = document.createElement("div");
+        const cardImg = document.createElement("img");
+        const cardSpan1 = document.createElement("span");
 
 
-
-        cardDiv2.textContent = `${data.bootstrap[key].headline}`;
-        cardDiv3.textContent = `${data.bootstrap[key].authorName}`
-        cardDiv2.textContent = `${data.bootstrap[key].headline}`;
-        cardDiv3.textContent = `${data.bootstrap[key].authorName}`
-        cardImg.setAttribute("src", data.bootstrap[key].authorPhoto)
-
-
-        for (let [key, value] of Object.entries(data.javascript)) {
+        cardDiv2.classList.add("headline");
+        cardDiv3.classList.add("author");
+        cardDiv4.classList.add("img-container");
 
 
 
+        cardDiv1.appendChild(cardDiv2);
+        cardDiv1.appendChild(cardDiv3);
+        cardDiv3.appendChild(cardDiv4);
+        cardDiv4.appendChild(cardImg);
+        cardDiv3.appendChild(cardSpan1);
 
-            cardDiv2.textContent = `${data.javascript[key].headline}`;
-            cardDiv3.textContent = `${data.javascript[key].authorName}`
-            cardDiv2.textContent = `${data.javascript[key].headline}`;
-            cardDiv3.textContent = `${data.javascript[key].authorName}`
-            cardImg.setAttribute("src", data.javascript[key].authorPhoto)
-        }
+        cardDiv2.textContent = `${javascriptvalue.headline}`;
+        cardImg.setAttribute("src", javascriptvalue.authorPhoto)
+        cardSpan1.textContent = ` by ${javascriptvalue.authorName}`
+    })
 
-        for (let [key, value] of Object.entries(data.jquery)) {
-
-
-
-
-            cardDiv2.textContent = `${data.jquery[key].headline}`;
-            cardDiv3.textContent = `${data.jquery[key].authorName}`
-            cardDiv2.textContent = `${data.jquery[key].headline}`;
-            cardDiv3.textContent = `${data.jquery[key].authorName}`
-            cardImg.setAttribute("src", data.jquery[key].authorPhoto)
-        }
-
-        for (let [key, value] of Object.entries(data.node)) {
+    data1.jquery.forEach((jqueryvalue) => {
+        const cardDiv1 = document.createElement("div");
+        cardDiv1.classList.add("card");
+        cardContainer.appendChild(cardDiv1)
 
 
+        const cardDiv2 = document.createElement("div");
+        const cardDiv3 = document.createElement("div");
+        const cardDiv4 = document.createElement("div");
+        const cardImg = document.createElement("img");
+        const cardSpan1 = document.createElement("span");
 
 
-            cardDiv2.textContent = `${data.node[key].headline}`;
-            cardDiv3.textContent = `${data.node[key].authorName}`
-            cardDiv2.textContent = `${data.node[key].headline}`;
-            cardDiv3.textContent = `${data.node[key].authorName}`
-            cardImg.setAttribute("src", data.node[key].authorPhoto)
-        }
-
-        for (let [key, value] of Object.entries(data.technology)) {
+        cardDiv2.classList.add("headline");
+        cardDiv3.classList.add("author");
+        cardDiv4.classList.add("img-container");
 
 
 
+        cardDiv1.appendChild(cardDiv2);
+        cardDiv1.appendChild(cardDiv3);
+        cardDiv3.appendChild(cardDiv4);
+        cardDiv4.appendChild(cardImg);
+        cardDiv3.appendChild(cardSpan1);
+
+        cardDiv2.textContent = `${jqueryvalue.headline}`;
+        cardImg.setAttribute("src", jqueryvalue.authorPhoto)
+        cardSpan1.textContent = ` by ${jqueryvalue.authorName}`;
+
+    });
+
+    data1.node.forEach((nodevalue) => {
+        const cardDiv1 = document.createElement("div");
+        cardDiv1.classList.add("card");
+        cardContainer.appendChild(cardDiv1)
 
 
-            cardDiv2.textContent = `${data.technology[key].headline}`;
-            cardDiv3.textContent = `${data.technology[key].authorName}`
-            cardDiv2.textContent = `${data.technology[key].headline}`;
-            cardDiv3.textContent = `${data.technology[key].authorName}`
-            cardImg.setAttribute("src", data.technology[key].authorPhoto)
-        }
+        const cardDiv2 = document.createElement("div");
+        const cardDiv3 = document.createElement("div");
+        const cardDiv4 = document.createElement("div");
+        const cardImg = document.createElement("img");
+        const cardSpan1 = document.createElement("span");
+
+
+        cardDiv2.classList.add("headline");
+        cardDiv3.classList.add("author");
+        cardDiv4.classList.add("img-container");
+
+
+
+        cardDiv1.appendChild(cardDiv2);
+        cardDiv1.appendChild(cardDiv3);
+        cardDiv3.appendChild(cardDiv4);
+        cardDiv4.appendChild(cardImg);
+        cardDiv3.appendChild(cardSpan1);
+
+        cardDiv2.textContent = `${nodevalue.headline}`;
+        cardImg.setAttribute("src", nodevalue.authorPhoto)
+        cardSpan1.textContent = ` by ${nodevalue.authorName}`;
+    })
+
+    data1.technology.forEach((technologyvalue) => {
+        const cardDiv1 = document.createElement("div");
+        cardDiv1.classList.add("card");
+        cardContainer.appendChild(cardDiv1)
+
+
+        const cardDiv2 = document.createElement("div");
+        const cardDiv3 = document.createElement("div");
+        const cardDiv4 = document.createElement("div");
+        const cardImg = document.createElement("img");
+        const cardSpan1 = document.createElement("span");
+
+
+        cardDiv2.classList.add("headline");
+        cardDiv3.classList.add("author");
+        cardDiv4.classList.add("img-container");
+
+
+
+        cardDiv1.appendChild(cardDiv2);
+        cardDiv1.appendChild(cardDiv3);
+        cardDiv3.appendChild(cardDiv4);
+        cardDiv4.appendChild(cardImg);
+        cardDiv3.appendChild(cardSpan1);
+        cardDiv2.textContent = `${technologyvalue.headline}`;
+        cardImg.setAttribute("src", technologyvalue.authorPhoto)
+        cardSpan1.textContent = ` by ${technologyvalue.authorName}`;
+
+    })
 
 }
 
-*/
-}
+
 const savedAxios = axios.get("https://lambda-times-backend.herokuapp.com/articles")
 
 .then(response => {
 
+    console.log(response)
 
-        return cardCreater(response["data"]["articles"])
+    for (let [key, value] of Object.entries(response["data"]["articles"])) {
+        const savedData1 = new cardCreater(response["data"]["articles"]);
+        cardContainer.appendChild(savedData1);
 
+    }
+})
 
-    })
-    .catch(err => {
-        console.log("it didnt work !")
-    })
+.catch(err => {
+    console.log("it didnt work !")
+
+})
