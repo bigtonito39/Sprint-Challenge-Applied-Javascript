6880a1320e675a779281e32e48193f26914ec84a
bigtonito39 <bigtonito@hotmail.com>
2019-10-14 22:28:31 -0400

    Adding final touches to strech

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,7 +1,7 @@
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to write a function that creates the carousel component, you will find the HTML below.
     2. You will need to grab a reference to all of the images
-    3. Create a current index
+    3. Create a current index  //try =+ carousel 
     4. Those buttons are gonna need some click handlers.
     5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
@@ -69,13 +69,13 @@ function carouselMaker() {
 
     caroudiv1.addEventListener("click", () => {
         caroudiv2.classList.toggle("left-button")
-        caroudiv2.classList.toggle("left-button")
+
 
 
 
     })
 
-    caroudiv1.addEventListener("click", (event) => {
+    caroudiv1.addEventListener("click", () => {
         caroudiv3.classList.toggle("right-button")
 
     })
